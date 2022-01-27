6bcf974686a8aa91e4d90b86956389654d6f8f62
sushantgwr87 <sushantgangwar87@gmail.com>
2022-01-28 00:23:10 +0530

    Added 404 Page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'next/link';
+
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Error 404</h2>
+            <p>This page cannot be found or is not build yet.</p>
+            <Link href="/" alt="home">Back to Homepage...</Link>
+        </div>
+    );
+};
+
+export default NotFound;
