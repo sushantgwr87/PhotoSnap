39db26fbaef7c704b26ad9d7ebee82f8354adf24
sushantgwr87 <sushantgangwar87@gmail.com>
2022-01-29 11:07:02 +0530

    Updated App.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
-import Layout from '../src/components/layout'
+import Layout from '../src/components/Layout'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <title>Photosnap | {Component.name}</title>
+        <meta name="description" content="A website to share life experiences of any place or a thing whichever you like with image creating your own story." />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
