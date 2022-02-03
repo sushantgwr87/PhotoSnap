884d89bb91ee88345ae8919997eec2a33cce4ec1
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-03 14:32:10 +0530

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,35 +27,31 @@ const Navbar = () => {
 	return (
 		<header>
 			<nav>
-				{/* <div> */}
-					<Link href="/">
-						<a>
-							<SVGIcon className={styles.logo} name="photosnap" fill={'#000'} width={'200px'} />
-						</a>
-					</Link>
-				{/* </div> */}
+				<Link href="/">
+					<a>
+						<SVGIcon className={styles.nav_logo} name="photosnap" fill={'#000'} width={'250px'} />
+					</a>
+				</Link>
 				{isMd ?
 					(
-						<div className={styles.navDesktop}>
+						<div className={styles.nav_desktop}>
 							<ul>
 								<Link href="/stories">STORIES</Link>
 								<Link href="/features">FEATURES</Link>
 								<Link href="/pricing">PRICING</Link>
 							</ul>
-							<button className={styles.inviteModalButton}>GET AN INVITE</button>
+							<button className={styles.nav_modal___btn}>GET AN INVITE</button>
 						</div>
 					)
 					:
 					<>
-						{/* <button className={styles.navMobileButton} onClick={() => setShowDropdown(b => !b)}> */}
-							<div className={showDropdown ? styles.hamburgerMenuClose : styles.hamburgerMenuOpen} onClick={() => setShowDropdown(b => !b)}></div>
-						{/* </button> */}
-						<div className={`${styles.navMobile} ${showDropdown ? styles.showSideNavbar : styles.hideSideNavbar}`} ref={dropdown}>
+						<div className={showDropdown ? styles.nav_mobile_menu___btn_close : styles.nav_mobile_menu___btn_open} onClick={() => setShowDropdown(b => !b)}></div>
+						<div className={`${styles.nav_mobile} ${showDropdown ? styles.nav_mobile___show : styles.nav_mobile___hide}`} ref={dropdown}>
 							<ul>
 								<li><Link href="/stories">STORIES</Link></li>
 								<li><Link href="/features">FEATURES</Link></li>
 								<li><Link href="/pricing">PRICING</Link></li>
-								<li><button className={styles.inviteModalButton}>GET IN INVITE</button></li>
+								<li><button className={styles.nav_modal___btn}>GET IN INVITE</button></li>
 							</ul>
 						</div>
 					</>
