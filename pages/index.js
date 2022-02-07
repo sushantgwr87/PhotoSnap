07decfd6723c930876a61fde92bd6b8b516fbbde
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-07 21:34:35 +0530

    Updated Index Page

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,82 @@
 import React from 'react';
-import Image from 'next/image';
 import SVGIcon from '../src/components/SVGIcon';
-import card_Image1 from '../public/assets/adventure.jpg';
-import card_Image2 from '../public/assets/camera.jpg';
-import card_Image3 from '../public/assets/video.jpg';
-import useBreakpoints from '../src/customHook/useBreakpoints';
+import cardImage1 from '../public/assets/adventure.jpg'
+import cardImage2 from '../public/assets/camera.jpg'
+import cardImage3 from '../public/assets/video.jpg'
 import HomeImageGrid from '../src/components/HomeImageGrid';
+import Card from '../src/components/card';
+
+const featuredata = [
+  {
+    icon: "responsive",
+    head: "100% Responsive",
+    body: "No matter which the device you're on, our site is fully responsive and stories look beautiful on any screen."
+  },
+  {
+    icon: "infinite",
+    head: "No photo upload limit",
+    body: "Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go."
+  },
+  {
+    icon: "social",
+    head: "Available to Embed",
+    body: "Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps and more."
+  }
+]
+
+const introductionData = [
+  {
+    path: cardImage1,
+    head: 'create and share your photo stories.',
+    body: 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
+    buttonText: 'Get an invite'
+  },
+  {
+    path: cardImage2,
+    head: 'Beautiful stories every time',
+    body: 'We provide design templates to ensure your stories look terrific. Easily add photos, text and embed media from other networks, Then share your story with everyone',
+    buttonText: 'View the Stories'
+  },
+  {
+    path: cardImage3,
+    head: 'Designed for Everyone',
+    body: 'Photosnap can help you create stories that resonates with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it.',
+    buttonText: 'View the Stories'
+  },
+]
 
 const Home = () => {
-  const { isSm } = useBreakpoints();
 
   return (
     <>
       <section className="introduction">
-        <div className="introduction_card">
-          <div className="introduction_card_photo">
-            <Image layout={isSm ? 'responsive' : 'fill'} src={card_Image1} alt='Card Image' />
-          </div>
-          <div className="introduction_card_content">
-            <h3>create and share your photo stories.</h3>
-            <p>Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>
-            <button>
-              <span>Get an invite</span>
-              <div className="arrow">
-                <SVGIcon name="arrow" fill={'#fff'} />
-              </div>
-            </button>
-          </div>
-        </div>
-        <div className="introduction_card">
-          <div className="introduction_card_photo">
-            <Image layout={isSm ? 'responsive' : 'fill'} src={card_Image2} alt='Card Image' />
-          </div>
-          <div className="introduction_card_content">
-            <h3>Beautiful stories every time</h3>
-            <p>We provide design templates to ensure your stories look terrific. Easily add photos, text and embed media from other networks, Then share your story with everyone</p>
-            <button>
-              <span>View the Stories</span>
-              <div className="arrow">
-                <SVGIcon name="arrow" fill={'#000'} />
-              </div>
-            </button>
-          </div>
-        </div>
-        <div className="introduction_card">
-          <div className="introduction_card_photo">
-            <Image layout={isSm ? 'responsive' : 'fill'} src={card_Image3} alt='Card Image' />
-          </div>
-          <div className="introduction_card_content">
-            <h3>Designed for Everyone</h3>
-            <p>Photosnap can help you create stories that resonates with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it.</p>
-            <button>
-              <span>View the Stories</span>
-              <div className="arrow">
-                <SVGIcon name="arrow" fill={'#000'} />
-              </div>
-            </button>
-          </div>
-        </div>
+        {introductionData.map((value, index) => (
+          <Card
+            imagePath={value.path}
+            cardHead={value.head}
+            cardBody={value.body}
+            isdark={index > 0 ? false : true}
+            isreverse={index == 1 && true}
+            buttonText={value.buttonText}
+            key={index}
+          />
+        ))
+        }
       </section>
       <section>
         <HomeImageGrid />
+      </section>
+      <section>
+        <div className='features'>
+          {featuredata.map((value, index) => (
+            <div className="feature" key={index}>
+              <SVGIcon name={value.icon} width='100px' height='100px' />
+              <h3>{value.head}</h3>
+              <p>{value.body}</p>
+            </div>
+          ))
+          }
+        </div>
       </section>
     </>
   );
