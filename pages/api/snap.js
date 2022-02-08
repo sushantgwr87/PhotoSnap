cfb3de326c3c38557820c97d2df7930fd1901756
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-09 01:45:22 +0530

    Added MongoDb Backened

---
+++ pages/api/snap.js
@@ -0,0 +1,23 @@
+const { connectToDatabase } = require('../../lib/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+export default async function handler(req, res) {
+    // switch the methods
+    switch (req.method) {
+        case 'GET': {
+            return getPosts(req, res);
+        }
+
+        case 'POST': {
+            return addPost(req, res);
+        }
+
+        case 'PUT': {
+            return updatePost(req, res);
+        }
+
+        case 'DELETE': {
+            return deletePost(req, res);
+        }
+    }
+}
