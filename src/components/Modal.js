eb0b07584d3018129d6b347269e4bced93e16664
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-10 01:17:39 +0530

    Updated Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,51 +1,59 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import SVGIcon from "./SVGIcon";
+import useMountTransition from "../customHook/useMountTransition";
 import styles from "../../styles/modal.module.css";
 
 const Modal = ({ show, onClose }) => {
-    const [isBrowser, setIsBrowser] = useState(false);
-    const closeOnEscapeKeyDown = e => {
-        if ((e.charCode || e.keyCode) === 27) {
-            onClose();
-        }
-    };
+  const [isBrowser, setIsBrowser] = useState(false);
+  const hasTransitionedIn = useMountTransition(show, 1000);
 
-    useEffect(() => {
-      setIsBrowser(true);
-      document.body.addEventListener("keydown", closeOnEscapeKeyDown);
-        return function cleanup() {
-            document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
-        };
-    }, []);
-
-    const handleCloseClick = (e) => {
-      e.preventDefault();
+  const closeOnEscapeKeyDown = e => {
+    if ((e.charCode || e.keyCode) === 27) {
       onClose();
-    };
+    }
+  };
 
-    const modalContent = show ? (
-      <div className={styles.modal_overlay}>
-        <div className={styles.modal}>
-          <div className={styles.modal_header}>
-            <a href="#" onClick={handleCloseClick}>
-              x
-            </a>
-          </div>
-          <div className={styles.modal_body}>
-              Hello Modal
-          </div>
+  useEffect(() => {
+    setIsBrowser(true);
+
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    return () => document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+  }, []);
+
+  const modalContent = 
+  (hasTransitionedIn ||show ? (
+      <div className={`${styles.modal_overlay} ${hasTransitionedIn && show ? styles.modal_show: styles.modal_hide}`} onClick={onClose}>
+      <div className={`${styles.modal} ${hasTransitionedIn && show ? styles.modal_show: styles.modal_hide}`} onClick={e => e.stopPropagation()}>
+        <div className={styles.modal_header}>
+          <SVGIcon name="sgwr" secondaryfill="#07b053" width="30%" />
+          <h3>Admin Login</h3>
+        </div>
+        <div className={styles.modal_body}>
+          <form className={styles.modal_body_form}>
+            <label>Username</label>
+            <input className={styles.modal_body_form___input} type="text" placeholder="Enter Username" name="username" required />
+            <label className={styles.modal_body_form___label}>Password</label>
+            <input className={styles.modal_body_form___input} type="password" placeholder="Enter Password" name="password" required />
+            <button className={styles.modal_body_form___button} type="submit">Login</button>
+          </form>
+        </div>
+        <div className={styles.modal_footer}>
+          <button className={styles.modal_footer_button} onClick={onClose}>Cancel</button>
+          <span>Info</span>
         </div>
       </div>
-    ) : null;
+    </div>
+  ) : null);
 
-    if (isBrowser) {
-        return ReactDOM.createPortal(
-          modalContent,
-          document.getElementById("modal-root")
-        );
-      } else {
-        return null;
-      }
+  if (isBrowser) {
+    return ReactDOM.createPortal(
+      modalContent,
+      document.getElementById("modal-root")
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Modal;
