dccf094d0d13d0e6a6ea54ad772b0427c0d17575
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-13 01:07:13 +0530

    Updated Card Component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import SVGIcon from './SVGIcon';
 import Image from 'next/image';
-import useBreakpoints from '../customHook/useBreakpoints';
 import styles from '../../styles/card.module.css';
 
 const Card = ({ imagePath, cardHead, cardBody, isbutton=true, isdark=true, isreverse=false, buttonText }) => {
-    const { isSm } = useBreakpoints();
 
     return (
         <div className={`${styles.card} ${isdark ? styles.card___dark : styles.card___light} ${isreverse && styles.card___reverse}`}>
             <div className={styles.card_photo}>
-                <Image layout={isSm ? "responsive" : "fill"} src={imagePath} alt='Card Image' />
+                <Image layout="fill" src={imagePath} alt='Card Image' />
             </div>
             <div className={styles.card_content}>
                 <h3>{cardHead}</h3>
