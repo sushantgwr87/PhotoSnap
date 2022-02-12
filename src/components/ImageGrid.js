88981521ebe2cbd0811f8674d3825f3b76cae09c
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-13 01:07:40 +0530

    Added Image Grid Component

---
+++ src/components/ImageGrid.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../../styles/Grid.module.css';
+import SVGIcon from './SVGIcon';
+import Link from 'next/link';
+
+const ImageGrid = ({ gridData }) => {
+  
+  return (
+    <div className={styles.image_grid_container}>
+      {gridData.map((value, index) => (
+        <div className={styles.image_grid___link} key={index}>
+          <Link href="/story" passHref>
+            <a>
+              <div className={styles.image_grid} key={index}>
+                <div className={styles.grid_photo}>
+                  <Image layout='fill' src={value.path} alt='Grid Image' />
+                </div>
+                <div className={styles.grid_content}>
+                  <h3>{value.title}</h3>
+                  <p>by {value.author}</p>
+                  <hr />
+                  <Link href="/story" passHref>
+                    <a className={styles.grid_content___read_btn}>
+                      <span>Read Story</span>
+                      <div className="arrow">
+                        <SVGIcon name="arrow" fill={'#fff'} />
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+      ))
+      }
+    </div>
+  );
+};
+
+{/* <button className={styles.grid_content___read_btn}>
+  <span>Read Story</span>
+  <div className="arrow">
+    <SVGIcon name="arrow" fill={'#fff'} />
+  </div>
+</button> */}
+export default ImageGrid;
