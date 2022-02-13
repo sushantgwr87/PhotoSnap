355625019d49458981eb3f3ad83ede91ee4468fa
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-14 01:02:39 +0530

    Updated Index Form

--- pages/uploadForm/index.js
+++ pages/uploadForm/index.js
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import Image from "next/image";
+import addImage from "../../public/assets/addImage.png";
 
-export default function PrivatePage(props) {
-  const [image, setImage] = useState(null);
-  const [createObjectURL, setCreateObjectURL] = useState(null);
+const PrivatePage = () => {
+  const [image, setImage] = useState(addImage);
+  const [createObjectURL, setCreateObjectURL] = useState(addImage);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    body: "",
+  });
+
+  const { title, author, body } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
   const uploadToClient = (event) => {
     if (event.target.files && event.target.files[0]) {
       const i = event.target.files[0];
-
       setImage(i);
       setCreateObjectURL(URL.createObjectURL(i));
     }
   };
 
-  const uploadToServer = async (event) => {        
+  const uploadToServer = async (event) => {
     const body = new FormData();
     // console.log("file", image)
-    body.append("file", image);    
+    body.append("file", image);
     const response = await fetch("/api/upload", {
       method: "POST",
       body
@@ -25,19 +36,64 @@ export default function PrivatePage(props) {
   };
 
   return (
-    <div>
-      <div>
-        <Image src={createObjectURL} alt="upload Image" />
-        <h4>Select Image</h4>
-        <input type="file" name="myImage" onChange={uploadToClient} />
-        <button
-          className="btn btn-primary"
-          type="submit"
-          onClick={uploadToServer}
-        >
-          Send to server
-        </button>
-      </div>
-    </div>
+    <>
+      <section className="upload">
+        <h2>Add New Story</h2>
+        <form>
+          <div className="upload_form_image">
+            <label htmlFor="imgUpload">
+              <div className="upload_image">
+                <Image src={createObjectURL} layout="fill" alt="upload Image" />
+              </div>
+            </label>
+            <input
+              type="file"
+              id="imgUpload"
+              name="storyImage"
+              onChange={uploadToClient}
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+          </div>
+          <div className="upload_content">
+            <label htmlFor="title">Title</label>
+            <input
+              name="title"
+              id="title"
+              type="text"
+              placeholder="Enter Title"
+              value={title}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <label htmlFor="body">Body</label>
+            <textarea
+              name="body"
+              id="body"
+              placeholder="Enter Body"
+              cols="30" rows="10"
+              value={body}
+              onChange={(e) => onChange(e)}
+              required
+            />
+            <label htmlFor="author">Author</label>
+            <input
+              name="author"
+              id="author"
+              type="text"
+              placeholder="Enter Author"
+              value={author}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <button type="submit" onClick={uploadToServer}>
+            Upload to server
+          </button>
+        </form>
+      </section>
+    </>
   );
 }
+
+export default PrivatePage;
