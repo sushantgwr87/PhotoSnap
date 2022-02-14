c026635c0594a378bf8a9b23d4b975fddf3a7df9
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-15 00:15:17 +0530

    Updated Form Page

--- pages/uploadForm/index.js
+++ pages/uploadForm/index.js
@@ -25,74 +25,104 @@ const PrivatePage = () => {
     }
   };
 
-  const uploadToServer = async (event) => {
+  const uploadToServer = async (e) => {
+    e.preventDefault();
+
     const body = new FormData();
-    // console.log("file", image)
-    body.append("file", image);
+    body.append("image", image);
+
     const response = await fetch("/api/upload", {
       method: "POST",
       body
     });
+    const imageData = await response.json();
+
+    if (imageData.ok) {
+      // let snapData = {
+      //   ...formData,
+      //   path: imageData.path,
+      //   createdAt: new Date().toLocaleString('en-US', { day: 'numeric', year: 'numeric', month: 'short', })
+      // }
+
+      // let res = await fetch('/api/snaps', {
+      //   method: 'POST',
+      //   body: JSON.stringify(snapData),
+      // });
+
+      // let data = await res.json();
+
+      // if (data.success) {
+      //   // reset the fields
+      //   console.log("done");
+      //   // set the message
+      // }
+
+      
+    }
   };
 
   return (
-    <>
-      <section className="upload">
-        <h2>Add New Story</h2>
-        <form>
-          <div className="upload_form_image">
-            <label htmlFor="imgUpload">
-              <div className="upload_image">
-                <Image src={createObjectURL} layout="fill" alt="upload Image" />
-              </div>
-            </label>
-            <input
-              type="file"
-              id="imgUpload"
-              name="storyImage"
-              onChange={uploadToClient}
-              accept=".png, .jpg, .jpeg"
-              hidden
-            />
-          </div>
-          <div className="upload_content">
-            <label htmlFor="title">Title</label>
-            <input
-              name="title"
-              id="title"
-              type="text"
-              placeholder="Enter Title"
-              value={title}
-              onChange={(e) => onChange(e)}
-              required
-            />
-            <label htmlFor="body">Body</label>
-            <textarea
-              name="body"
-              id="body"
-              placeholder="Enter Body"
-              cols="30" rows="10"
-              value={body}
-              onChange={(e) => onChange(e)}
-              required
-            />
-            <label htmlFor="author">Author</label>
-            <input
-              name="author"
-              id="author"
-              type="text"
-              placeholder="Enter Author"
-              value={author}
-              onChange={(e) => onChange(e)}
-              required
-            />
-          </div>
-          <button type="submit" onClick={uploadToServer}>
-            Upload to server
-          </button>
-        </form>
-      </section>
-    </>
+    // <>
+    <section className="upload">
+      <h2>Add New Story</h2>
+      <form onSubmit={uploadToServer} className="upload_form">
+        <div className="upload_form_image">
+          <label htmlFor="imgUpload">
+            <div className="upload_image">
+              <Image src={createObjectURL} layout="fill" alt="upload Image" />
+            </div>
+          </label>
+          <input
+            type="file"
+            id="imgUpload"
+            name="storyImage"
+            onChange={uploadToClient}
+            accept=".png, .jpg, .jpeg"
+            hidden
+          // required
+          />
+        </div>
+        <div className="upload_content">
+          <label htmlFor="title">Title</label>
+          <input
+            className="upload_content___input"
+            name="title"
+            id="title"
+            type="text"
+            placeholder="Enter Title"
+            value={title}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <label htmlFor="body">Body</label>
+          <textarea
+            className="upload_content___input"
+            name="body"
+            id="body"
+            placeholder="Enter Body"
+            cols="30" rows="10"
+            value={body}
+            onChange={(e) => onChange(e)}
+            required
+          />
+          <label htmlFor="author">Author</label>
+          <input
+            className="upload_content___input"
+            name="author"
+            id="author"
+            type="text"
+            placeholder="Enter Author"
+            value={author}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <button className="upload_form___btn" type="submit">
+          Upload to server
+        </button>
+      </form>
+    </section>
+    // </>
   );
 }
 
