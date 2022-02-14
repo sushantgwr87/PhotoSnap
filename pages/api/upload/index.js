46f1557c5da8e7fef2bd2be6b66cedb9d2002bef
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-15 00:14:58 +0530

    Updated Upload API

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -1,34 +1,33 @@
-import { IncomingForm } from 'formidable'
-import { promises as fs } from 'fs'
+import { IncomingForm } from 'formidable';
+import { v4 as uuid } from 'uuid';
 
 var mv = require('mv');
-var id = 0;
 export const config = {
     api: {
-       bodyParser: false,
+        bodyParser: false,
     }
 };
- 
+
 export default async function UploadImage(req, res) {
-    
+
+    const unique_id = uuid();
+
     const data = await new Promise((resolve, reject) => {
-       const form = new IncomingForm()
-       
+        const form = new IncomingForm()
+
         form.parse(req, (err, fields, files) => {
             if (err) return reject(err)
-            // console.log(fields, files)
-            var oldPath = files.file.filepath;
-            var newPath = `./public/upload/${files.file.originalFilename}`;
-            mv(oldPath, newPath, function(err) {});
-            // console.log(files.file.filepath);
-            // console.log(newPath);
-            res.status(200).json({ fields, files })
+            resolve({ fields, files });
         })
-        
-    })
-    const contents = fs.readFile(data?.files?.nameOfTheInput.path, {
-        encoding: 'utf8',
-    })
-    console.log(contents);
-    
+    });
+    try {
+        var oldPath = data.files.image.filepath;
+        var newPath = `./public/upload/image_${unique_id}.jpg`;
+        mv(oldPath, newPath, function (err) { });
+        res.status(200).json({ ok: true, message: "Image Uploaded", path: newPath.replace("./public", "") })
+    }
+    catch (error) {
+        res.status(500).json({ ok: false, message: error.message });
+        return;
+    }
 }
