d2cec3e70fddd61ad93f67e625fa7a7cf53417c1
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-22 00:33:28 +0530

    Updated Layout Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,33 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
-    const [displayChildren, setDisplayChildren] = useState(children);
-    const [transitionStage, setTransitionStage] = useState("fadeOut");
-    useEffect(() => {
-        setTransitionStage("fadeIn");
-    }, []);
-
-    useEffect(() => {
-        if (children !== displayChildren)
-            setTransitionStage("fadeOut");
-    }, [children, displayChildren]);
 
     return (
         <>
             <Navbar />
             <div className='container'>
-                <main
-                    onTransitionEnd={() => {
-                        if (transitionStage === "fadeOut") {
-                          setDisplayChildren(children);
-                          setTransitionStage("fadeIn");
-                        }
-                      }}
-                      className={`content ${transitionStage}`}
-                >
-                    {displayChildren}
+                <main>
+                    {children}
                 </main>
                 <Footer />
             </div>
