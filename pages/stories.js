0ee3a7aaf6ba79640492005a4978dd67ced8b1d5
sushantgwr87 <sushantgangwar87@gmail.com>
2022-02-21 08:05:56 +0530

    Updated Stories Page

--- pages/stories.js
+++ pages/stories.js
@@ -1,29 +1,26 @@
-import React from 'react';
+import React, { useEffect,useRef } from 'react';
 import Image from 'next/image';
 import ImageGrid from '../src/components/ImageGrid';
 import SVGIcon from '../src/components/SVGIcon';
 import Link from 'next/link';
 
-const featuredStory = {
-  id: 1,
-  body: "All. Air them seas saying evening male rule. Our beginning light seas that life morning was Moveth. Brought two man created which waters. Behold. Gathering good fill given. Saw own you're yielding let have. Under brought open creepeth creeping creature gathered Own let. One Blessed isn't sea. Behold upon void lesser together. Rule. Upon, sea day won't herb. Image beast you're light bring saying third tree man sea meat waters, greater creature won't void of abundantly blessed set above them after cattle spirit blessed brought fill. And bearing night cattle us upon be god wherein firmament life darkness, fruitful. Replenish.",
-  title: "hazy full moon of appalachia",
-  author: "Joseph D'zuza",
-  path: "/upload/adventure.jpg"
-}
-
 const Stories = ({snaps}) => {
+
+  const featuredSnap = snaps[0];
+
+  const otherSnaps =  snaps.filter((val,index)=> index>0);
+
   return (
     <>
       <section className='stories_featured'>
         <div className='stories_featured_image'>
-          <Image layout='fill' src={'/assets/adventure.jpg'} alt='Story_Image' />
+          <Image layout='fill' src={featuredSnap.path} alt='Story_Image' />
         </div>
         <div className='stories_featured_content'>
-          <h3>{featuredStory.title}</h3>
-          <span>by {featuredStory.author}</span>
-          <p>{featuredStory.body && featuredStory.body.length > 300 ? `${featuredStory.body.substring(0, 300)}...` : featuredStory.body}</p>
-          <Link href={'/story/'+featuredStory.id} passHref>
+          <h3>{featuredSnap.title}</h3>
+          <span>by {featuredSnap.author}</span>
+          <p>{featuredSnap.body && featuredSnap.body.length > 300 ? `${featuredSnap.body.substring(0, 300)}...` : featuredSnap.body}</p>
+          <Link href={'/story/'+featuredSnap._id} passHref>
             <a>
               <div className='stories_featured_content___read_btn'>
                 <span>Read the Story</span>
@@ -36,7 +33,7 @@ const Stories = ({snaps}) => {
         </div>
       </section>
       <section>
-        <ImageGrid gridData={snaps} />
+        <ImageGrid gridData={otherSnaps} />
       </section>
     </>
   );
